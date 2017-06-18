69b219998b6b8cddbe5fe5413b8a5447d9ea4916
Veeru Home <veerupotru@outlooke.com>
2017-06-18 21:44:13 +0530

    bootstrap page and links designed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule,Routes } from "@angular/router";
-import { HomeComponent } from "./home.component";
-import { PagenotfoundComponent } from "./pagenotfound.component";
-import { ErrorComponent } from "./error.component";
+import { HomeComponent } from "./home/home.component";
+import { PagenotfoundComponent } from "./pagenotfound/pagenotfound.component";
+import { ErrorComponent } from "./error/error.component";
+import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
+  {path: 'login', component : LoginComponent},
   {path: 'home', component : HomeComponent},
   {path: 'error', component : ErrorComponent},
   {path: 'pagenotfound', component : PagenotfoundComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from './app.component';
-import { PagenotfoundComponent } from './pagenotfound.component';
-import { ErrorComponent } from './error.component';
-import { HomeComponent } from './home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PagenotfoundComponent,
     ErrorComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule, AppRoutingModule
